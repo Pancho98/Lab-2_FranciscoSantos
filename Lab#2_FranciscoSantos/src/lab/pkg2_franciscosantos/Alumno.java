5e1fb0f0ec2a611bda5b24b49ae0be1a16cac1d2
Francisco Santos <fjgsantos98@gmail.com>
2017-10-20 13:34:03 -0600

    La clase Clase y alumno

--- Lab#2_FranciscoSantos/src/lab/pkg2_franciscosantos/Alumno.java
+++ Lab#2_FranciscoSantos/src/lab/pkg2_franciscosantos/Alumno.java
@@ -10,5 +10,91 @@ package lab.pkg2_franciscosantos;
  * @author Francisco Santos
  */
 public class Alumno {
+    private String nombre;
+    private int cuenta;
+    private String carrera;
+    private int edad;
+    private int dinero;
+    private String usuario;
+    private String contraseña;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Alumno(String nombre, int cuenta, String carrera, int edad, int dinero, String usuario, String contraseña) {
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+        this.carrera = carrera;
+        this.edad = edad;
+        this.dinero = dinero;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(int cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(int dinero) {
+        this.dinero = dinero;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno\n" + "nombre=" + nombre;
+    }
+    
     
 }
--- Lab#2_FranciscoSantos/src/lab/pkg2_franciscosantos/Clase.java
+++ Lab#2_FranciscoSantos/src/lab/pkg2_franciscosantos/Clase.java
@@ -18,4 +18,70 @@ public class Clase {
     private String maestro;
     private int uv;
     ArrayList<Alumno> lista = new ArrayList();
+
+    public Clase() {
+    }
+
+    public Clase(String nombre, String seccion, int max, String maestro, int uv) {
+        this.nombre = nombre;
+        this.seccion = seccion;
+        this.max = max;
+        this.maestro = maestro;
+        this.uv = uv;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSeccion() {
+        return seccion;
+    }
+
+    public void setSeccion(String seccion) {
+        this.seccion = seccion;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public String getMaestro() {
+        return maestro;
+    }
+
+    public void setMaestro(String maestro) {
+        this.maestro = maestro;
+    }
+
+    public int getUv() {
+        return uv;
+    }
+
+    public void setUv(int uv) {
+        this.uv = uv;
+    }
+
+    public ArrayList<Alumno> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Alumno> lista) {
+        this.lista = lista;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase" + "nombre=" + nombre;
+    }
+    
+    
 }
